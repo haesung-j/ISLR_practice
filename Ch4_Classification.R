470e2b55f9ba0a4d1a9c25df2f7425e799dc0db9
haesung <wjdgotjd0808@gmail.com>
2019-01-31 20:07:43 +0900

    Add code Ch4_Classfication

--- Ch4_Classification.R
+++ Ch4_Classification.R
@@ -8,3 +8,22 @@ summary(Smarket)          # Direction is qulitative, ow are quantitative.
 cor(Smarket[, -9])        # Only Year,Volumn have substantial correlation
 attach(Smarket)
 plot(Year, Volume)
+
+
+#### Logistic Regression
+# Fit model to predict Direction using Lag1 ~ Lag5 and Volume
+glm.fit <- glm(Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5+Volume, data=Smarket, family = binomial)
+summary(glm.fit)           # Smallest p-value : Lag1 but 0.14 & negative coefficient
+coef(glm.fit)              # Access the coefficients for fitted model
+summary(glm.fit)$coef
+
+glm.probs <- predict(glm.fit, type='response')  # output probs of the form P(Y=1|X)
+head(glm.probs)
+contrasts(Direction)            # R has created dummy variable with a 1 for Up
+
+glm.pred <- rep('Down',nrow(Smarket))   # If predict probs>0.5, We predict Direction is Up
+glm.pred[glm.probs>0.5] <- 'Up'
+
+table(glm.pred, Direction)      # confusion matrix
+mean(glm.pred == Direction)     # 0.5216
+
