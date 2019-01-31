b16a125e3fb24c6aa86aa08a773b1e2b6fcbb400
haesung <wjdgotjd0808@gmail.com>
2019-01-31 20:57:12 +0900

    Add code Ch4_Classification.R

--- Ch4_Classification.R
+++ Ch4_Classification.R
@@ -49,3 +49,61 @@ mean(glm.pred == Direction.2005)  # 0.5595238
 
 ## prediction with particular values of predictor
 predict(glm.fit, newdata=data.frame(Lag1=c(1.2,1.5),Lag2=c(1.1,-0.8)), type='response')
+
+
+#### Linear Discriminant Analysis(LDA) ####
+# fit LDA model using lda(), which is part of MASS library
+library(MASS)
+lda.fit <- lda(Direction~Lag1+Lag2, data=Smarket, subset=train)
+lda.fit     # it gives Prior probs, Group means, Coefficients of LD
+plot(lda.fit)
+
+# predict() returns a list : Class, Posterior, x
+lda.pred <- predict(lda.fit, Smarket.2005)
+names(lda.pred)
+
+lda.class <- lda.pred$class
+table(lda.class, Direction.2005)
+mean(lda.class == Direction.2005)     # 0.5595238
+
+# the result is gained by using 50% threshold to posterior probs
+sum(lda.pred$posterior[,1]>0.5)     # 70    - Down
+sum(lda.pred$posterior[,1]<=0.5)    # 182   - Up
+lda.pred$posterior[1:20,]          # Notice that the posterir probs output coreespond to the probs of Down
+
+# If you want to use other threshold, 0.9
+sum(lda.pred$posterior[,1]>0.9)   # 0  -> result : No Down
+
+#### Quadratic Discriminant Analysis(QDA) ####
+# using qda() which is also part of MASS library
+qda.fit <- qda(Direction~Lag1+Lag2, data=Smarket, subset=train)
+qda.fit        # it doesn't give the coefficients
+
+qda.pred <- predict(qda.fit, newdata=Smarket.2005)
+qda.class <- qda.pred$class
+table(qda.class, Direction.2005)
+mean(qda.class == Direction.2005) # 0.5992063
+
+
+#### K-Nearest Neighbors (KNN) ####
+# using knn(), which is part of the class library - it needs 4 inputs
+# A matrix containing the predictors associated with the train set, labeled train.X
+# A matrix containing the predictios associated with the test set, labeled test.X
+# A vector containing the class label for the training set, labeled train.Direction below
+# A value for K, the number of nearest neighbors to be used by the classifier
+#
+# We predict Direction using Lag1, Lag2
+
+library(class)
+train.X <- cbind(Lag1,Lag2)[train,]
+test.X <- cbind(Lag1,Lag2)[!train,]
+train.Direction <- Direction[train]
+
+set.seed(1)
+knn.pred <- knn(train.X, test.X, train.Direction, k=1)
+table(knn.pred ,Direction.2005)
+mean(knn.pred == Direction.2005)   # 0.5  --- K=1 model is very flexible
+
+knn.pred <- knn(train.X, test.X, train.Direction, k=3)
+table(knn.pred, Direction.2005)
+mean(knn.pred == Direction.2005)   # 0.5357143
